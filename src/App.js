e7f29b804c86ac541365eec38bc69773bdd39f5c
mia24sims <167013397+mia24sims@users.noreply.github.com>
2025-10-09 10:20:06 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import logo from "./shecodes-logo.avif"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
