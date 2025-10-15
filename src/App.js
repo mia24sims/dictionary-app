2c143b62f5bea602ac1ce3d513d81462a82b9e22
mia24sims <167013397+mia24sims@users.noreply.github.com>
2025-10-15 12:21:46 -0500

    Added Dictionary component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
-import logo from "./shecodes-logo.avif"
+import logo from "./shecodes-logo.avif";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small> Coded by Tamia Sims</small>
+        </footer>
+      </div>
     </div>
   );
 }
