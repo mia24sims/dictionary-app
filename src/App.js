e1852221db69082aee8291a43fb4000307f34628
mia24sims <167013397+mia24sims@users.noreply.github.com>
2025-10-22 21:32:36 -0500

    added links for github and netlify

--- src/App.js
+++ src/App.js
@@ -5,15 +5,19 @@ function App() {
   return (
     <div className="App">
       <div className="App">
-        <header className="App-header">
-        Dictionary App
-        </header>
+        <header className="App-header">Dictionary App</header>
         <main>
-          <Dictionary defaultKeyword="rainbow"/>
+          <Dictionary defaultKeyword="rainbow" />
         </main>
         <footer className="App-footer">
-          <small> Coded by Tamia Sims, open-sourced on <a href="">Github</a>,
-          and hosted on <a href="">Netlify</a>
+          <small>
+            {" "}
+            Coded by Tamia Sims, open-sourced on{" "}
+            <a href="https://github.com/mia24sims/dictionary-app">Github</a>,
+            and hosted on{" "}
+            <a href="https://dictionary-app-shecodes-bymia.netlify.app/">
+              Netlify
+            </a>
           </small>
         </footer>
       </div>
